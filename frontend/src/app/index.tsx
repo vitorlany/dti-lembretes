cee1cfe46578fd193e5322340d0cb477a1aa0d75
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2024-02-08 00:37:40 -0300

    base tela

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,10 +1,32 @@
-import './index.sass'
+import './index.module.scss'
+
+import { FormEvent } from 'react';
+
+const handleCriar = async (event: FormEvent) => {
+  event.preventDefault()
+}
 
 function index() {
-
   return (
     <>
-      <p>Em construção :)</p>
+      <h1>Novo lembrete</h1>
+      <form onSubmit={handleCriar}>
+        <div>
+          <label htmlFor="nome">Nome</label>
+          <input type="text" id="nome" />
+        </div>
+        <div>
+          <label htmlFor="data">Data</label>
+          <input type="date" id="data" />
+        </div>
+        <button type="submit">Criar</button>
+      </form>
+      {/*  */}
+      <h1>Lista de lembretes</h1>
+      <article>
+        <time dateTime="2024-01-01">01/01/2023</time>
+        <p>Vídeo de estatística</p>
+      </article>
     </>
   )
 }
