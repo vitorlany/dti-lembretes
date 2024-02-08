449e4e6f609b210afa809182a51dc79855e12227
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2024-02-08 08:50:30 -0300

    react hook form

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,30 +1,32 @@
 import './index.module.scss'
 
-import { FormEvent } from 'react';
+import { FieldValues, useForm } from "react-hook-form";
 
-const handleCriar = async (event: FormEvent) => {
-  event.preventDefault()
+const handleCriar = async (fields: FieldValues) => {
+  console.log(fields)
 }
 
 function index() {
+  const { register, handleSubmit } = useForm();
+
   return (
     <>
       <h1>Novo lembrete</h1>
-      <form onSubmit={handleCriar}>
+      <form onSubmit={handleSubmit(handleCriar)}>
         <div>
           <label htmlFor="nome">Nome</label>
-          <input type="text" id="nome" />
+          <input type="text" {...register("nome")} />
         </div>
         <div>
           <label htmlFor="data">Data</label>
-          <input type="date" id="data" />
+          <input type="date" {...register("data")} />
         </div>
         <button type="submit">Criar</button>
       </form>
       {/*  */}
       <h1>Lista de lembretes</h1>
       <article>
-        <time dateTime="2024-01-01">01/01/2023</time>
+        <time dateTime="2024-01-01">01/01/2024</time>
         <p>Vídeo de estatística</p>
       </article>
     </>
