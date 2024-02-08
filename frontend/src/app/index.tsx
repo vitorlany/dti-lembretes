ac155e40a183f3c2638ba905fca97b806072d356
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2024-02-08 11:50:20 -0300

    listar lembretes

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,18 +1,9 @@
 import "./index.module.scss";
 
 import { useState } from "react";
-import moment from "moment";
 import { Lembrete } from "../types/lembrete";
 import NovoLembrete from "../components/NovoLembrete/index.tsx";
-
-let calendarFormat = {
-  sameDay: "[Today], DD",
-  nextDay: "[Tomorrow], DD",
-  nextWeek: "dddd",
-  lastDay: "[Yesterday]",
-  lastWeek: "[Last] dddd",
-  sameElse: "DD/MM/YYYY",
-};
+import ListarLembretes from "../components/ListarLembretes/index.tsx";
 
 function index() {
   const [lembretes, setLembretes] = useState<Lembrete[]>([]);
@@ -30,15 +21,7 @@ function index() {
   return (
     <>
       <NovoLembrete onCLick={addLembrete} />
-      <h1>Lista de lembretes</h1>
-      {lembretes.map(({ data, nome }) => (
-        <article>
-          <time dateTime={moment(data).format("YYYY-MM-DD")}>
-            {moment(data).calendar(null, calendarFormat)}
-          </time>
-          <p>{nome}</p>
-        </article>
-      ))}
+      <ListarLembretes lembretes={lembretes} />
     </>
   );
 }
---
+++ frontend/src/components/ListarLembretes/index.tsx
@@ -0,0 +1,34 @@
+import { Lembrete } from "../../types/lembrete";
+import moment from "moment";
+
+let calendarFormat = {
+  sameDay: "[Today], DD",
+  nextDay: "[Tomorrow], DD",
+  nextWeek: "dddd",
+  lastDay: "[Yesterday]",
+  lastWeek: "[Last] dddd",
+  sameElse: "DD/MM/YYYY",
+};
+
+interface Props {
+  lembretes: Lembrete[];
+}
+
+function index(props: Props) {
+  let lembretes = props.lembretes;
+  return (
+    <div>
+      <h1>Lista de lembretes</h1>
+      {lembretes.map(({ data, nome }) => (
+        <article>
+          <time dateTime={moment(data).format("YYYY-MM-DD")}>
+            {moment(data).calendar(null, calendarFormat)}
+          </time>
+          <p>{nome}</p>
+        </article>
+      ))}
+    </div>
+  );
+}
+
+export default index;
