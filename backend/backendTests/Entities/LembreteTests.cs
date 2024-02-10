7fcdfeed7d9ea1f0f3f479967c675036f8aebcdb
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2024-02-10 11:32:06 -0300

    Teste unitário

---
+++ backend/backendTests/Entities/LembreteTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace backend.Entity.Tests
+{
+    [TestClass()]
+    public class LembreteTests
+    {
+        [TestMethod]
+        public void Lembrete_CriarComNomeVazio()
+        {
+            string name = "";
+            DateOnly date = DateOnly.FromDateTime(DateTime.Now.AddDays(1));
+
+            Assert.ThrowsException<ArgumentException>(() => new Lembrete(name, date));
+        }
+
+        [TestMethod]
+        public void Lembrete_CriarComDataPassada()
+        {
+            string name = "Teste";
+            DateOnly date = DateOnly.FromDateTime(DateTime.Now.AddDays(-1));
+
+            Assert.ThrowsException<ArgumentException>(() => new Lembrete(name, date));
+        }
+
+        [TestMethod]
+        public void Lembrete_CriarComDadosValidos_CriaLembrete()
+        {
+            string name = "Teste";
+            DateOnly date = DateOnly.FromDateTime(DateTime.Now.AddDays(1));
+
+            var lembrete = new Lembrete(name, date);
+
+            Assert.AreEqual(name, lembrete.Name);
+            Assert.AreEqual(date, lembrete.Date);
+        }
+    }
+}
